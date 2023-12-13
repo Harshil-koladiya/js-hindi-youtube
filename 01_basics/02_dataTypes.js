93abfca3967627835d6f5f71286393b745490edf
Harshil-koladiya <148050408+Harshil-koladiya@users.noreply.github.com>
2023-12-13 05:26:16 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+3)   // we are using nodejs, not browser
+
+let name = "harshil"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53 (range)
+//bigInt
+//string => ""
+//boolean => true/false
+//null => stand-alone value
+//undefined => value is not assign to variable
+//symbol => unique
+
+
+
+//object
+console.log(typeof undefined); //undefined
+console.log(typeof null);  //object
