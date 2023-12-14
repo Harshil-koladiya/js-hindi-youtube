0bf0a6d2076754c0f87141beec6a6b39610df289
Harshil-koladiya <148050408+Harshil-koladiya@users.noreply.github.com>
2023-12-14 16:19:38 +0000

    conversion of datatype done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,6 +24,6 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
-console.log(stringNumber);
+console.log(typeof stringNumber);   // =>string
+console.log(stringNumber);   // =>33
 
