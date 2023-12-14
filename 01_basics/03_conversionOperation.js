2407ea4a59fba9376d767aa9e219eea27ac663b3
Harshil-koladiya <148050408+Harshil-koladiya@users.noreply.github.com>
2023-12-14 17:03:44 +0000

    done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,8 +1,8 @@
 let score = "33abc"
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 //"33" => 33
@@ -15,7 +15,7 @@ console.log(valueInNumber);
 let isLoggedIn = ""
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 //"" => false
@@ -24,6 +24,31 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber);   // =>string
-console.log(stringNumber);   // =>33
+// console.log(typeof stringNumber);   // =>string
+// console.log(stringNumber);   // =>33
+
+ // ****************** Operations *****************
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = "harshil"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2); // =>12
+// console.log(1 + "2"); // =>12
+// console.log("1" + 2 + 2); // =>122
+// console.log(1 + 2 + "2"); // =>32
+
+
+
+
+
 
