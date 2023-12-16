31087ae3951122f69a08f42b73c0e9d1a0b3637b
Harshil-koladiya <148050408+Harshil-koladiya@users.noreply.github.com>
2023-12-16 05:09:18 +0000

    datatype-summary

--- 01_basics/datatype-summary.js
+++ 01_basics/datatype-summary.js
@@ -2,10 +2,41 @@
 
 // 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
 
+const name = "harshil"
+  
+const score = 100
+const scoreValue = 100.03
 
+const isLoggedIn = false
+const outSideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);  // =>false
+
+const bigNumber = 354662884902n
 
 
 
 //Reference (Non primitive)
 
 //Array, Objects, Functions
+
+const heros = ["shaktiman", "nagraj", "doga"];
+
+let myObj = {
+    name : "harshil",
+    age : 18,
+}
+
+const myFunction = function(){
+    console.log("hello world");
+}
+
+// typeof
+//primitive ma je type hoy te j ave example string nu string, number nu number, undefined nu undefined etc
+// but null no type object ave ane bigInt no type undefined ave
+
+// non-primitive ma badha no type object j ave
+// function => object function ave
