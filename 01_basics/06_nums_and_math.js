33ca508e9b78f7be7cb29241b393a6986f227bee
Harshil-koladiya <148050408+Harshil-koladiya@users.noreply.github.com>
2023-12-17 10:18:09 +0000

    nums and math

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,40 @@
+const score = 400 // =>type-1
+console.log(score); // =>400
+
+
+const balance = new Number(100) // =>type-2
+console.log(balance); // => [Number: 100]
+
+console.log(balance.toString().length); // =>3
+console.log(balance.toFixed(2)); // =>100.00
+// => point pachi ketla value sudhi value joye che te ape 
+
+
+
+const otherNumber = 123.8966
+
+console.log(otherNumber.toPrecision(4)); // =>123.9
+console.log(otherNumber.toPrecision(3)); // =>124
+
+
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN')); // =======> 10,00,000
+
+
+
+
+//+++++++++++++++++++++ Maths +++++++++++++++++++++++++
+
+// console.log(Math.abs(-4));    // => 4
+// console.log(Math.abs(4));     // => 4
+// console.log(Math.round(4.6)); // => 5
+// console.log(Math.ceil(4.2));  // => 5
+// console.log(Math.floor(4.9)); // => 4
+// console.log(Math.min(4, 5, 3, 8)); // => 3
+// console.log(Math.max(4, 5, 3, 8)); // => 8
+
+
+console.log(Math.random());  // => value 0 to 1 ni vache ave 
+console.log(Math.random()*10); // => 0 to 10 ni vache ave
+console.log(Math.random()*10 + 1); // => 1 to 10 ni vache ave
