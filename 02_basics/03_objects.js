9fb977ee373c4b423e10e956aaee96c79437c9c0
Harshil-koladiya <148050408+Harshil-koladiya@users.noreply.github.com>
2023-12-23 04:48:32 +0000

    object

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -5,7 +5,7 @@
 
 // object literals
 
-const mysym = Symbol() // =>symbole ne define karvu pade object ma use karva pela
+const mysym = Symbol(mykey) // =>symbole ne define karvu pade object ma use karva pela
 
 
 const Jsuser = {
@@ -29,15 +29,20 @@ Jsuser.email = "harshilkoladiya.com"
 Jsuser.email = "harshil.com"
 // console.log(Jsuser); // freeze pachi nu change no thay object ma
 
-
+// jsuser ma function add karyu greeting name api
 Jsuser.greeting = function(){
     console.log("hello js user");
 }
 
+
+// jsuser ma function add karyu greetingTwo name api
 Jsuser.greetingTwo = function(){
     console.log(`hello js user, ${this.name}`);
 }
-
+ // this no use te j object ma koy parameter  hoy ane tema j define karla function ma fari thi  use karvo hoy as refrence tyare tene   this.___ no use thay
 
 console.log(Jsuser.greeting()); // => hello js user
 console.log(Jsuser.greetingTwo());// => hello js user, harshil
+
+// function ne print karvu hoy tyare je name ma save karyu te name ni pachal () lagavu upper example apya che
+
