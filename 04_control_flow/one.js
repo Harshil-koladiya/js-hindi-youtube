5b2bc60d67ffede84137e26b7c1fe6aa0c5fe83c
Harshil-koladiya <148050408+Harshil-koladiya@users.noreply.github.com>
2023-12-25 13:24:15 +0000

    done

---
+++ 04_control_flow/one.js
@@ -0,0 +1,23 @@
+// if
+// const temperature = 41
+
+
+// if(temperature < 50){
+//     console.log("less than 50");
+// }else{
+//     console.log('temperature is greater than 50');
+// }
+
+
+// <, >, <=, >=, ==, !=, ===, !==
+
+
+// const score = 200
+
+// if(score > 100){
+//     const power = "fly"
+//     console.log(`user power: ${power}`);
+// }
+
+// console.log(`user power: ${power}`); // =>gives scope error(power is not defined)
+
