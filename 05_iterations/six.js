f843c1a30ab3a06fbc38992c9f58fbaaca9954fa
Harshil-koladiya <148050408+Harshil-koladiya@users.noreply.github.com>
2023-12-31 07:11:57 +0000

    done

---
+++ 05_iterations/six.js
@@ -0,0 +1,41 @@
+// const coding = ["js", "ruby", "java", "python", "cpp"]
+
+
+// const values = coding.forEach( (item) => {
+//     console.log(item );
+//     return item
+// })
+
+// console.log(values); // => undefined reason is ( foreach koy pan 
+// kale value return no kare atle values ma kay store j nay to te 
+// undefined ave atle j filter no use thay)
+
+// filter a foreach ni jem darek value mate fxn call kare ane value pan return kare
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+// M - 1
+// const newNums = myNums.filter( (num) => num > 4)
+
+
+// M - 2
+// const newNums = myNums.filter( (num) => {
+//     return num > 4  // => explicit return fxn ma avi gayelu che
+// }) 
+
+
+
+// M - 3
+// const newNums = []
+
+// myNums.forEach( (num) => {
+//     if(num > 4) {
+//         newNums.push(num)
+//     }
+// })
+
+// console.log(newNums); // => [5, 6, 7, 8, 9, 10]
+
+
+
