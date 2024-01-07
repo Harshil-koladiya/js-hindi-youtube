609a63fa426813457e39048c530a0d2fa1f797ab
Harshil-koladiya <148050408+Harshil-koladiya@users.noreply.github.com>
2024-01-07 16:21:04 +0000

    done

--- 05_iterations/nine.js
+++ 05_iterations/nine.js
@@ -1,1 +1,36 @@
-h
+const myNums = [1, 2, 3]
+
+// const myTotal = myNums.reduce(function (acc, currval) {
+//     console.log(`acc: ${acc} and currval: ${currval}`);
+//     return acc + currval
+// }, 0)
+
+
+const myTotal = myNums.reduce( (acc, currval) => acc + currval,0)
+
+console.log(myTotal); // => 6
+
+
+const shoppingCart = [
+    
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 3999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+const priceToPay = shoppingCart.reduce( (acc, item) => acc + item.price,0)
+
+console.log(priceToPay); // => 25996
